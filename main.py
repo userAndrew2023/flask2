be66a3b08b2c0e54c771bb315c6e10b15fddaba5
Jopkins <justfredbear@gmail.com>
2024-02-15 16:45:44 +0300

    commit

--- main.py
+++ main.py
@@ -12,5 +12,22 @@ def index():
     return render_template('index.html', **data)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    if "инженер" in prof or "строитель" in prof:
+        prof = "Инженерные тренажеры"
+    else:
+        prof = "Научные симуляторы"
+    data = {
+        "prof": prof,
+        "image": f"/static/img/{prof}.jpg"
+    }
+    return render_template('training.html', **data)
+
+
+def error404():
+    return "УПС. Эта страница не найдена"
+
+
 if __name__ == '__main__':
     app.run()
